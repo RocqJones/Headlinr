568dc22e8c37c4ac9af8102ca7a95e5d21cc86d8
RocqJones <jonesmbindyo@gmail.com>
2025-05-07 20:18:36 +0300

    size

--- androidApp/src/main/java/com/zonkesoft/headlinr/android/ui/screens/SplashScreen.kt
+++ androidApp/src/main/java/com/zonkesoft/headlinr/android/ui/screens/SplashScreen.kt
@@ -38,7 +38,7 @@ fun SplashScreen(navController: NavHostController) {
             Image(
                 painter = painterResource(id = R.mipmap.logo),
                 contentDescription = stringResource(R.string.icon),
-                modifier = Modifier.size(width = 150.dp, height = 150.dp)
+                modifier = Modifier.size(width = 300.dp, height = 300.dp)
             )
         }
     }
