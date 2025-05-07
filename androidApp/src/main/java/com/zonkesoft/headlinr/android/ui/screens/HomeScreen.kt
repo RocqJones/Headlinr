f69ae0902390c96c99972f574bce13a51cfe873c
RocqJones <jonesmbindyo@gmail.com>
2025-05-07 20:21:52 +0300

    Android: Preview HomeScreen

--- androidApp/src/main/java/com/zonkesoft/headlinr/android/ui/screens/HomeScreen.kt
+++ androidApp/src/main/java/com/zonkesoft/headlinr/android/ui/screens/HomeScreen.kt
@@ -1,8 +1,34 @@
 package com.zonkesoft.headlinr.android.ui.screens
 
+import android.content.res.Configuration
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+import com.zonkesoft.headlinr.android.ui.theme.MyApplicationTheme
 
 @Composable
 fun HomeScreen(navController: NavHostController) {
+}
+
+@Preview(
+    showSystemUi = true,
+    showBackground = true,
+    widthDp = 450,
+    uiMode = Configuration.UI_MODE_NIGHT_NO,
+    name = "Light"
+)
+@Preview(
+    showSystemUi = true,
+    showBackground = true,
+    widthDp = 450,
+    uiMode = Configuration.UI_MODE_NIGHT_YES,
+    name = "Dark"
+)
+
+@Composable
+fun HomeScreenPreview() {
+    MyApplicationTheme {
+        HomeScreen(rememberNavController())
+    }
 }
