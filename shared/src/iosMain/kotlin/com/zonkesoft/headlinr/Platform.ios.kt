b086e51645d56ad0be0904163f8363135bc07f3a
RocqJones <jonesmbindyo@gmail.com>
2025-07-23 15:35:25 +0300

    Platform iOS: api-key from local

--- shared/src/iosMain/kotlin/com/zonkesoft/headlinr/Platform.ios.kt
+++ shared/src/iosMain/kotlin/com/zonkesoft/headlinr/Platform.ios.kt
@@ -1,11 +1,17 @@
 package com.zonkesoft.headlinr
 
 import platform.Foundation.NSBundle
+import platform.Foundation.NSDictionary
+import platform.Foundation.dictionaryWithContentsOfFile
 import platform.UIKit.UIDevice
 
 class IOSPlatform() : Platform {
     override val name: String = UIDevice.currentDevice.systemName() + " " + UIDevice.currentDevice.systemVersion
-    override val apiKey: String = NSBundle.mainBundle.objectForInfoDictionaryKey("NEWS_API_KEY") as? String ?: ""
+
+    val dict = NSDictionary.dictionaryWithContentsOfFile(
+        NSBundle.mainBundle.pathForResource("Secrets", "plist") ?: ""
+    ) as NSDictionary?
+    override val apiKey: String = dict?.objectForKey("NEWS_API_KEY") as? String ?: ""
 }
 
 actual fun getPlatform(): Platform = IOSPlatform()
